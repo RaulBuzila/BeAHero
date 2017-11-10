72c56a0e711f1323633095bd9987a6ddf5f0ab9b
alinciuciu05 <33542837+alinciuciu05@users.noreply.github.com>
2017-11-10 13:57:43 +0200

    Meniu lateral

--- app/src/main/java/com/example/raulbuzila/beahero/HomeActivity.java
+++ app/src/main/java/com/example/raulbuzila/beahero/HomeActivity.java
@@ -80,17 +80,17 @@ public class HomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_istoric) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_account) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_info) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_centre) {
+
+        } else if (id == R.id.nav_contact) {
 
         }
 
